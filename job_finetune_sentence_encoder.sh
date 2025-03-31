8c555eafa797a77ffa3254269f53a8a069aa5895
Marcus Tantakoun <mtantakoun@gmail.com>
2025-03-31 09:32:37 -0400

    purged

--- job_finetune_sentence_encoder.sh
+++ job_finetune_sentence_encoder.sh
@@ -10,6 +10,7 @@
 #SBATCH --error=./slurm_out/fine-tune-%j.err
 #SBATCH --account=rrg-zhu2048
 
+module purge
 module load StdEnv/2023
 ml cuda python/3.11 arrow/17.0.0
 
@@ -18,7 +19,7 @@ pip install --upgrade pip --no-index
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 
-pip install --no-index torch scikit_learn tqdm nltk torchtext transformers>=4.43.1 spacy triton accelerate datasets scipy matplotlib numpy==1.23.5 huggingface_hub ipython sentence-transformers
+pip install --no-index torch scikit_learn tqdm nltk torchtext transformers>=4.43.1 spacy triton accelerate datasets scipy matplotlib numpy huggingface_hub ipython sentence-transformers
 
 # Environment variables
 export MASTER_ADDR=$(hostname)
