dac86d37b49816d038d319b9f02f4fc92c059584
Marcus Tantakoun <mtantakoun@gmail.com>
2025-03-31 09:26:39 -0400

    testing

--- finetune.py
+++ finetune.py
@@ -5,33 +5,33 @@ from datetime import datetime
 from pathlib import Path
 
 
-if __name__ == "__main__":
-    
-    # save path of model
-    output_dir = os.path.join("data/03_models", f"finetuned_sentence_encoder_batch_test_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}")
-    print(f"Model will be saved at: {output_dir}")
-    Path(output_dir).mkdir(parents=True, exist_ok=True)
-    
-    file_path = os.path.join(output_dir, "training_log.txt")
+print("THIS IS WORKING")
 
-    # Write some content into the file
-    with open(file_path, "w") as f:
-        f.write("Training started...\n")
+# save path of model
+output_dir = os.path.join("data/03_models", f"finetuned_sentence_encoder_batch_test_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}")
+print(f"Model will be saved at: {output_dir}")
+Path(output_dir).mkdir(parents=True, exist_ok=True)
 
-    installed_packages = [pkg.key for pkg in pkg_resources.working_set]
-    print(installed_packages)
-    
-    setup_sentence_encoder_cfg = {
-        "model_name": "/home/tant2002/scratch/codebert-base",
-        "model_type": "bi_encoder",
-        "device": "cuda",  # Change to "cuda" if using GPU
-        "is_evaluated": False
-    }
+file_path = os.path.join(output_dir, "training_log.txt")
 
-    finetuning_encoder_cfg = {
-        "train_batch_size": 256,
-        "training_epoch": 40,
-        "is_finetune_complete": False
-    }
+# Write some content into the file
+with open(file_path, "w") as f:
+    f.write("Training started...\n")
 
-    train_sentence_encoder(setup_sentence_encoder_cfg=setup_sentence_encoder_cfg, finetuning_encoder_cfg=finetuning_encoder_cfg)
+installed_packages = [pkg.key for pkg in pkg_resources.working_set]
+print(installed_packages)
+
+setup_sentence_encoder_cfg = {
+    "model_name": "/home/tant2002/scratch/codebert-base",
+    "model_type": "bi_encoder",
+    "device": "cuda",  # Change to "cuda" if using GPU
+    "is_evaluated": False
+}
+
+finetuning_encoder_cfg = {
+    "train_batch_size": 256,
+    "training_epoch": 40,
+    "is_finetune_complete": False
+}
+
+train_sentence_encoder(setup_sentence_encoder_cfg=setup_sentence_encoder_cfg, finetuning_encoder_cfg=finetuning_encoder_cfg)
