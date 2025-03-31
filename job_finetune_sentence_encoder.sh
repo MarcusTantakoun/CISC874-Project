779b97262ca1d5278e4b12b94586b3202a04219c
Marcus Tantakoun <mtantakoun@gmail.com>
2025-03-31 09:06:33 -0400

    removed requirements to script

--- job_finetune_sentence_encoder.sh
+++ job_finetune_sentence_encoder.sh
@@ -18,8 +18,7 @@ pip install --upgrade pip --no-index
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 
-pip install -r requirements.txt
-pip install --no-index torch scikit_learn tqdm nltk torchtext transformers>=4.43.1 spacy triton accelerate datasets scipy matplotlib numpy huggingface_hub ipython
+pip install --no-index torch scikit_learn tqdm nltk torchtext transformers>=4.43.1 spacy triton accelerate datasets scipy matplotlib numpy huggingface_hub ipython sentence-transformers
 
 # Environment variables
 export MASTER_ADDR=$(hostname)
